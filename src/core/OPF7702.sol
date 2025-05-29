28a3da9b20e605d598bf9018d65957d9b74c75ca
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-05-29 11:04:28 +0200

    fix: correct override on batch execute

--- src/core/OPF7702.sol
+++ src/core/OPF7702.sol
@@ -31,6 +31,16 @@ import {
     _packValidationData
 } from "lib/account-abstraction/contracts/core/Helpers.sol";
 
+    struct Transaction {
+        address to;
+        uint256 value;
+        bytes data;
+    }
+    
+interface IExtendedAccount {
+    function execute(Transaction[] calldata txs) external payable;
+}
+
 /**
  * @title Openfort Base Account 7702 with ERC-4337 Support
  * @author Openfort@0xkoiner
@@ -126,7 +136,7 @@ contract OPF7702 is KeysManager, Initializable, ReentrancyGuard, WebAuthnVerifie
      * @dev Can only be called via EntryPoint or by self
      * @param _transactions Array of transactions to execute
      */
-    function execute(Transaction[] calldata _transactions) external payable nonReentrant {
+    function execute(Transaction[] calldata _transactions) external payable virtual nonReentrant {
         _requireForExecute();
         if (_transactions.length == 0 || _transactions.length > 9) {
             revert OpenfortBaseAccount7702V1__InvalidTransactionLength();
