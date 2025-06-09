ce678ba54bf705446a690b3a175febe3a5d316a0
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-06-09 12:50:45 +0200

    tests upd

--- test/unit/Execution.t.sol
+++ test/unit/Execution.t.sol
@@ -17,7 +17,7 @@ import {WebAuthnVerifier} from "src/utils/WebAuthnVerifier.sol";
 import {PackedUserOperation} from
     "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
-contract RegistartionTest is Base {
+contract Execution is Base {
     /* ───────────────────────────────────────────────────────────── contracts ── */
     IEntryPoint public entryPoint;
     WebAuthnVerifier public webAuthn;
