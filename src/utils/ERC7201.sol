6698dc40f2ee86545243d08fac2ade607c2db9c9
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-06-13 18:13:36 +0200

    forge fmt

--- src/utils/ERC7201.sol
+++ src/utils/ERC7201.sol
@@ -9,7 +9,8 @@ contract ERC7201 is IERC7201 {
     /// @notice Storage root computed in compliance with ERC-7201 standard
     /// @dev This constant value is hardcoded in CaliburEntry.sol for compile-time determinism.
     /// It is equal to: keccak256(abi.encode(uint256(keccak256("openfort.baseAccount.7702.v1")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 public constant CUSTOM_STORAGE_ROOT = 0xeddd36aac8c71936fe1d5edb073ff947aa7c1b6174e87c15677c96ab9ad95400;
+    bytes32 public constant CUSTOM_STORAGE_ROOT =
+        0xeddd36aac8c71936fe1d5edb073ff947aa7c1b6174e87c15677c96ab9ad95400;
 
     /// @notice Returns the namespace and version of the contract
     function namespaceAndVersion() external pure returns (string memory) {
