ab81506190d0b491694d5750880e4f3731824915
0xkoiner <alexgray.bcn.esp@gmail.com>
2025-10-20 13:00:54 +0200

    (test) Increase % Branches in src/core/BaseOPF7702.sol    71.43% (5/7)

--- test/by-contract/BaseOPF7702Test.t.sol
+++ test/by-contract/BaseOPF7702Test.t.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.29;
 
 import {Deploy} from "./../Deploy.t.sol";
 import {console2 as console} from "lib/forge-std/src/Test.sol";
+import {BaseOPF7702} from "src/core/BaseOPF7702.sol";
+import {PackedUserOperation} from
+    "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
 import "src/interfaces/IERC7821.sol";
 import "lib/openzeppelin-contracts/contracts/interfaces/IERC1271.sol";
@@ -47,5 +50,64 @@ contract BaseOPF7702Test is Deploy {
     function test_tokensReceived() public {
         _etch();
         account.tokensReceived(makeAddr("operator"), makeAddr("from"), makeAddr("to"), 1000, "", "");
+    }
+
+    function test_setEntryPointRevertsWhenSame() public {
+        _etch();
+        vm.startPrank(address(account));
+        vm.expectRevert(BaseOPF7702.BaseOPF7702__NoChangeUpdateContractAddress.selector);
+        account.setEntryPoint(address(entryPoint));
+        vm.stopPrank();
+    }
+
+    function test_setEntryPointUpdatesAddress() public {
+        _etch();
+        address newEntryPoint = makeAddr("newEntryPoint");
+        vm.startPrank(address(account));
+        account.setEntryPoint(newEntryPoint);
+        vm.stopPrank();
+        assertEq(address(account.entryPoint()), newEntryPoint);
+    }
+
+    function test_setWebAuthnVerifierRevertsWhenSame() public {
+        _etch();
+        vm.startPrank(address(account));
+        vm.expectRevert(BaseOPF7702.BaseOPF7702__NoChangeUpdateContractAddress.selector);
+        account.setWebAuthnVerifier(WEBAUTHN_VERIFIER);
+        vm.stopPrank();
+    }
+
+    function test_setWebAuthnVerifierUpdatesAddress() public {
+        _etch();
+        address newVerifier = makeAddr("newVerifier");
+        vm.startPrank(address(account));
+        account.setWebAuthnVerifier(newVerifier);
+        vm.stopPrank();
+        assertEq(account.webAuthnVerifier(), newVerifier);
+    }
+
+    function test_setGasPolicyRevertsWhenSame() public {
+        _etch();
+        vm.startPrank(address(account));
+        vm.expectRevert(BaseOPF7702.BaseOPF7702__NoChangeUpdateContractAddress.selector);
+        account.setGasPolicy(address(gasPolicy));
+        vm.stopPrank();
+    }
+
+    function test_setGasPolicyUpdatesAddress() public {
+        _etch();
+        address newPolicy = makeAddr("newPolicy");
+        vm.startPrank(address(account));
+        account.setGasPolicy(newPolicy);
+        vm.stopPrank();
+        assertEq(account.gasPolicy(), newPolicy);
+    }
+
+    function test_validateUserOpRevertsWhenNotEntryPoint() public {
+        _etch();
+        PackedUserOperation memory userOp;
+        userOp.sender = address(account);
+        vm.expectRevert(BaseOPF7702.NotFromEntryPoint.selector);
+        account.validateUserOp(userOp, bytes32(0), 0);
     }
 }
